f792aae4c4f27443879878cf5b6c80e2a41d6816
Diego Katz <diekatz@gmail.com>
2023-04-04 19:27:57 -0300

    se agrego metodo divideNumeros

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 using System;
 
-namespace C#
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args0)
+        {
+
+            Console.WriteLine(divideNumeros(18, 7));
+
+        }
+        static double divideNumeros(double num1, int num2)
+        {
+            double resultado
+            resultado = num1 / num2;
+            return resultado;
+        }
+    }
+}
+
